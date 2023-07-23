b156bad6d88af3aeb5de8c9e65dc10e932c297c4
TALIS-PRO <khalidtahiritkt@gmail.com>
2023-07-23 11:47:55 +0100

    add print_int.c & mdfy main.h

--- main.h
+++ main.h
@@ -6,7 +6,7 @@ int _printf(const char *format, ...);
 int print_format(char format, va_list ap);
 int print_char(int c);
 int print_string(char *str);
-int printf_int(va_list args);
-int printf_dec(va_list args);
+int print_int(va_list args);
+int print_dec(va_list args);
 
 #endif
--- print_int.c
+++ print_int.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * printf_int - prints integer
+ * print_int - prints integer
  *
  * @args: argument to print
- * Return: nbber of characters printed
+ * Return: number of characters printed
  */
-int printf_int(va_list args)
+int print_int(va_list args)
 {
 	int n = va_arg(args, int);
 	int nb, last = n % 10, dgt, exp = 1;
-	int  i = 1;
+	int i = 1;
 
 	n = n / 10;
 	nb = n;
@@ -17,71 +18,73 @@ int printf_int(va_list args)
 	if (last < 0)
 	{
 		_putchar('-');
-	 nb =  nb;
+		nb = -nb;
 		n = -n;
 		last = -last;
 		i++;
 	}
-	if  nb > 0)
+
+	if (nb > 0)
 	{
-		while  nb / 10 != 0)
+		while (nb / 10 != 0)
 		{
 			exp = exp * 10;
-		 nb = nb / 10;
+			nb = nb / 10;
 		}
-	 nb = n;
+		nb = n;
 		while (exp > 0)
 		{
 			dgt = nb / exp;
 			_putchar(dgt + '0');
-		 nb = nb - (dgt * exp);
+			nb = (dgt * exp);
 			exp = exp / 10;
 			i++;
 		}
 	}
+
 	_putchar(last + '0');
 
 	return (i);
 }
 
 /**
- * printf_dec - prints decimal
+ * print_dec - prints decimal
  *
- * @args: argument to print
- * Return: nbber of characters printed
+ * @args: arguments to print
+ * Return: number of characters printed
  */
-
-int printf_dec(va_list args)
+int print_dec(va_list args)
 {
 	int n = va_arg(args, int);
 	int nb, last = n % 10, dgt;
-	int  i = 1;
+	int i = 1;
 	int exp = 1;
 
 	n = n / 10;
- nb = n;
+	nb = n;
 
 	if (last < 0)
 	{
 		_putchar('-');
-	 nb =  nb;
+		nb = -nb;
 		n = -n;
 		last = -last;
 		i++;
 	}
-	if  nb > 0)
+
+	if (nb > 0)
 	{
-		while  nb / 10 != 0)
+		while (nb / 10 != 0)
 		{
 			exp = exp * 10;
-		 nb = nb / 10;
+			nb = nb / 10;
 		}
-	 nb = n;
+		nb = n;
 		while (exp > 0)
 		{
 			dgt = nb / exp;
 			_putchar(dgt + '0');
-		 nb = nb - (dgt * exp);
+			nb = nb - (dgt * exp);
 			exp = exp / 10;
 			i++;
 		}
