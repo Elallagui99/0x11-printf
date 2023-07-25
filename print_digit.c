6e8514cbfa256aa845b56bfac6a4df3ead8620d7
Elallagui99 <got.femme@gmail.com>
2023-07-25 19:19:12 +0100

    fix some betty tyling

--- print_digit.c
+++ print_digit.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_int - print int
+ * print_digit - print int
  * @n: number to print
+ * @base: base on the digit
+ * @format: flages to print in order from
  * Return: number of number printed
  */
 
 int print_digit(int n, int base, char format)
 {
 	int count = 0;
-	char *sym = "0123456789abcdef";
+	char *sym;
 
 	if (format == 'x')
 		sym = "0123456789abcdef";
@@ -20,11 +22,12 @@ int print_digit(int n, int base, char format)
 		print_char('-');
 		return (print_digit(-n, base, format) + 1);
 	}
-	else if(n < base)
-		return(print_char(sym[n]));
+	else if (n < base)
+		return (print_char(sym[n]));
 	else
 	{
 		count = print_digit(n / base, base, format);
 		return (print_digit(n % base, base, format) + count);
 	}
+
 }
